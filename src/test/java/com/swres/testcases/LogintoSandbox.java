3972edb3652bc6edd94699253234112e7765631e
Sidd <harshu.nm1@gmail.com>
2025-02-01 16:12:57 +1100

    version 2.0

---
+++ src/test/java/com/swres/testcases/LogintoSandbox.java
@@ -0,0 +1,30 @@
+package com.swres.testcases;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LogintoSandbox {
+
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+
+        driver.get("https://www.sandbox.paypal.com");
+        
+
+        
+
+
+
+
+    }
+
+}
